9a3cf53e7681e2f58e89fc41bd1f7d14f0007268
Camila <macaroa123@gmail.com>
2025-08-01 13:38:55 +0000

    Removed cuda_visible_devices to use env variable.

--- M2_Artery_vein/test_outside.sh
+++ M2_Artery_vein/test_outside.sh
@@ -7,11 +7,11 @@ dataset_name='ALL-AV'
 test_checkpoint=1401
 
 date
-CUDA_VISIBLE_DEVICES=0 python test_outside.py --batch-size=8 \
-                                                --dataset=${dataset_name} \
-                                                --job_name=20210724_${dataset_name}_randomseed \
-                                                --checkstart=${test_checkpoint} \
-                                                --uniform=True
+python test_outside.py --batch-size=8 \
+                        --dataset=${dataset_name} \
+                        --job_name=20210724_${dataset_name}_randomseed \
+                        --checkstart=${test_checkpoint} \
+                        --uniform=True
 
 
 date
